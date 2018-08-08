0beae709a663cc87b6b3eb10d9b276a748a9ed90
AABoachie <aa944@njit.edu>
2018-08-08 14:44:05 -0400

    just another push to test webhook automation

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const port = 8080;
 const app = express();
 
 app.use('/', (req, res) =>{
-  res.send('My first Openshift app!!!!');
+  res.send('My first Openshift app!!!! Testing out webhooks, they are pretty neat.');
 });
 
-app.listen(port, () => {
+app.listen(port, ()
+=> {
   console.log(`Listening on port ${port}`);
 });
