84475d24a70ee72ee8b0dc32c5824398f7432247
AABoachie <aa944@njit.edu>
2018-08-08 14:50:39 -0400

    retry

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const express = require('express');
 const port = 8080;
 const app = express();
 
-app.use('/', (req, res) =>{
-  res.send('My first Openshift app!!!! Testing out webhooks, they are pretty neat.');
+app.use('/', (req, res) => {
+  res.send('My first Openshift app!!!! Testing out webhooks, they are pretty neat.')
 });
 
-app.listen(port, ()
-=> {
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
